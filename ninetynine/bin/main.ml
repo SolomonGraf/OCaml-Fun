932dd1ade04644c79e45d3bdf465fcaaa3423086
solacefountain <SolomonFGraf@gmail.com>
2024-01-02 03:00:38 -0500

    Worked on more of the ninety nine problems

--- ninetynine/bin/main.ml
+++ ninetynine/bin/main.ml
@@ -26,7 +26,7 @@ let rec ith list i =
   | v :: tail -> if i = 0 then Some v else ith tail (i-1)
 end
 
-;; print_bool (Some 3 = ith test_list 3)
+;; print_bool (Some 3 = ith test_list 2)
 
 let length list =
   let rec loop list acc =
@@ -73,5 +73,38 @@ let rec compress list =
   else x :: compress (y :: tail)
 end
 
-;; print_bool (compress [1;1;1;1;1;2;2;2;3;4;4;4;5;5] = test_list)
+;; print_bool (compress [1;1;1;1;1;2;2;2;3;4;4;4] = test_list)
+
+let pack list =
+  let rec loop list current acc =
+    begin match list with
+    | [] -> acc
+    | [x] -> (x :: current) :: acc
+    | x :: y :: tail -> if x = y then loop (y :: tail) (x :: current) acc
+                                 else loop (y :: tail) [] ((x :: current) :: acc)
+    end in List.rev (loop list [] [])
+
+;; print_bool (pack ["a"; "a"; "a"; "a"; "b"; "c"; "c"; "a"; "a"; "d"; "d"; "e"; "e"; "e"; "e"] =
+[["a"; "a"; "a"; "a"]; ["b"]; ["c"; "c"]; ["a"; "a"]; ["d"; "d"];
+ ["e"; "e"; "e"; "e"]])
+
+let encode list =
+  let rec loop list current acc =
+    begin match list with
+    | [] -> acc
+    | [x] -> (1 + current, x) :: acc
+    | x :: y :: tail -> if x = y 
+      then loop (y :: tail) (1 + current) acc
+      else loop (y :: tail) 0 ((current + 1, x) :: acc)
+    end in List.rev (loop list 0 [])
+
+;; print_bool ((encode ["a"; "a"; "a"; "a"; "b"; "c"; "c"; "a"; "a"; "d"; "e"; "e"; "e"; "e"]) =
+  [(4, "a"); (1, "b"); (2, "c"); (2, "a"); (1, "d"); (4, "e")])
+
+type 'a rle =
+  | One of 'a
+  | Many of int * 'a;;
+type 'a rle = One of 'a | Many of int * 'a
+
+(* encode modified im lazy *)
 
