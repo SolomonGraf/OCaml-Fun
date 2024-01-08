2328ac079555a079d01726e6f9a735c92b30d861
solacefountain <SolomonFGraf@gmail.com>
2024-01-07 20:29:11 -0500

    fixed up set interface

--- set.ml
+++ set.ml
@@ -1,24 +1,4 @@
-module type SET = sig
-
-  type 'a set
-
-  val empty : 'a set
-  val add : 'a -> 'a set -> 'a set
-  val remove : 'a -> 'a set -> 'a set
-  val member : 'a -> 'a set -> bool
-
-  val intersection : 'a set -> 'a set -> 'a set
-  val union : 'a set -> 'a set -> 'a set
-  val symmetric_difference : 'a set -> 'a set -> 'a set
-  val difference : 'a set -> 'a set -> 'a set
-  val powerset : 'a set -> 'a set set
-
-  val list_of_set : 'a set -> 'a list
-  val set_of_list : 'a list -> 'a set
-  
-end
-
-module SET : SET = struct
+module SET = struct
 
   type 'a set = 'a list
 
